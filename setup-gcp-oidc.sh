218f5be60f8f389701fe610fc558f64629edec30
chaowei@mba-m3 <gba115566@gmail.com>
2025-09-02 01:14:37 +0800

    fix: update OIDC setup with correct repository path

--- setup-gcp-oidc.sh
+++ setup-gcp-oidc.sh
@@ -12,7 +12,7 @@ set -e  # 遇到錯誤立即退出
 
 # 配置變數
 PROJECT_ID="ton-cat-lottery-dev-3"
-GITHUB_REPO="chaowei-liu/ton-cat-lottery"
+GITHUB_REPO="chaowei-dev/ton-cat-lottery"
 SERVICE_ACCOUNT_NAME="gha-deploy"
 WORKLOAD_IDENTITY_POOL_NAME="github-pool" 
 WORKLOAD_IDENTITY_PROVIDER_NAME="github-provider"
